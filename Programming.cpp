2a80cac6d8de0b7702307377430df4f9e8b5c05a
Artem01xx <131177303+Artem01xx@users.noreply.github.com>
2023-05-06 14:19:58 +0500

    Constr
    Constr

---
+++ Programming.cpp
@@ -0,0 +1,60 @@
+﻿// Programming.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class MyClass
+{
+private:
+	int* data;
+	int size;
+
+public:
+
+	MyClass(int size)
+	{
+		this->data = new int[size];
+		this->size = size;
+
+		for (int i = 0;i < size;i++)
+		{
+			data[i] = i;
+			cout << " Конструктор " << this << endl;
+		}
+	}
+
+	MyClass(const MyClass& other)
+	{
+		this->size = other.size;
+		this->data = new int[other.size];
+
+		for (int i = 0;i < size;i++)
+		{
+			data[i] = i;
+			cout << " Конструктор копирования " << this << endl;
+		}
+
+	}
+
+
+	~MyClass()
+	{
+		cout << " Деструктор " << this << endl;
+		delete[]data;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	MyClass a(1);
+	MyClass b(a);
+
+	return 0;
+}
